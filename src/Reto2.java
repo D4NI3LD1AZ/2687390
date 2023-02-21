612967cead27c11bc1683fb85e00dcddb5b09025
DanielD <danielfernandodiaz5962@gmail.com>
2023-02-21 09:27:32 -0500

    Se creo la carpeta Reto2

---
+++ src/Reto2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Reto2{
+    public static void main(String[] args) throws Exception {
+        Scanner lectura = new Scanner (System.in);
+    int edad;
+    double peso;
+    System.out.println("Ingrese la edad del bebe en meses: ");
+    edad= lectura.nextInt();
+    System.out.println("Ingrese el peso del bebe: ");
+    peso= lectura.nextInt();
+    double dosis=((peso+10)/(edad*10)*8);
+    System.out.println("La dosis que se le debe aplicar al bebe es de: " + dosis );
+    lectura.close();
+    }
+}
---
+++ src/Reto4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Reto4{
+    public static void main(String[] args) throws Exception{
+       boolean azar,moneda;
+       Scanner lectura=new Scanner (System.in);
+       Random aleatorio=new Random();
+       System.out.println("Escoja true para cara o false para cruz");
+       azar=lectura.nextBoolean();
+       moneda=aleatorio.nextBoolean();
+       if(moneda==true && azar==true){
+        System.out.println("Es el ganador al escoger cara");
+       }
+       else if(moneda==false && azar==false){
+        System.out.println("Es el ganador por esocger cruz");
+       }
+       else{
+        System.out.println("Usted ha perdido");
+       }
+    lectura.close();
+}
+}
