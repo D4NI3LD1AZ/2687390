4e522e6ff499499becd58a1dbd16fc9d4b47638c
DanielD <danielfernandodiaz5962@gmail.com>
2023-02-24 12:06:00 -0500

    Se actualizo la carpeta presupuesto

--- src/Presupuesto.java
+++ src/Presupuesto.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 public class Presupuesto{
     public static void main(String[] args) throws Exception {
         Scanner lectura = new Scanner (System.in);
-    int spent,budget,totals,op;
+    int spent,budget,totals,op,contador;
     op=1;
+    contador=1;
     budget=100000;
-    while (op==1)
+    while (op==1 && contador <=3)
     {
         System.out.println("Ingrese el gasto");
         spent =lectura.nextInt();
@@ -13,9 +14,9 @@ public class Presupuesto{
 
         System.out.println("El gasto es de = " + spent + " Y le queda de presupuesto = " + budget);
 
-
         System.out.println("Desea ingresar otro gasto 1.si 2.no");
         op=lectura.nextInt();
+        contador =contador+1;
     }
 
     lectura.close();
