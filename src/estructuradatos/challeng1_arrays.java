222f97e55be1ebf421deba909bfa2c89099702af
DanielD <danielfernandodiaz5962@gmail.com>
2023-03-01 08:46:45 -0500

    Se creo la carpeta challeng1

--- src/estructuradatos/challeng1_arrays.java
+++ src/estructuradatos/challeng1_arrays.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 public class challeng1_arrays {
     public static void main(String[] args) {
         Scanner lectura=new Scanner(System.in);
-        int numnotes;
-        double notas=0,promedio;
+        int notes;
+        double suma=0,promedio;
         System.out.println("Digite la cantidad de notas");
-        numnotes=lectura.nextInt();
-        double []amountnote=new double [numnotes];
+        notes=lectura.nextInt();
+        double []amountnote=new double [notes];
         for (int i=0;i<amountnote.length;i++){
-            System.out.println("Digite la nota");
+            System.out.println("Digite su nota");
             amountnote [i]=lectura.nextDouble();
-            notas=notas+amountnote[i];
+            suma=suma+amountnote[i];
         }
-        promedio=notas/numnotes;
+        promedio=suma/notes;
         lectura.close();
         if ( promedio<3.0){
             System.out.println("Su promedio fue:"+promedio);
-            System.out.println("Reprobaste la asignatura");
+            System.out.println("Reprobo la materiaa");
         }
         else if (promedio<4.0){
             System.out.println("Su promedio fue:"+promedio);
-            System.out.println("Pasaste raspando la asignatura");
+            System.out.println("Paso raspando la materia");
         }
         else if (promedio>4.0){
             System.out.println("Su promedio fue:"+promedio);
